2fcb0788b73dc2a9b79cfa79d4db4808043e37dc
Nerio Navea <neriojose5@gmail.com>
2017-11-05 19:50:24 -0400

    Setup react-rails gem

---
+++ app/javascript/components/Dashboard.js
@@ -0,0 +1,12 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+class Dashboard extends React.Component {
+
+  render(){
+    return(
+      <div>Hi im a component</div>
+    )
+  }
+}
+export default Dashboard
